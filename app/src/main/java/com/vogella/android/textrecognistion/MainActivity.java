50d6030024a5378f22815c509328c102ffc3ccfb
Udara Abeythilake <udara.i@eyepax.com>
2019-10-18 16:55:25 +0530

    first commit

---
+++ app/src/main/java/com/vogella/android/textrecognistion/MainActivity.java
@@ -0,0 +1,106 @@
+package com.vogella.android.textrecognistion;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    private Bitmap myBitmap;
+    private ImageView myImageView;
+    private TextView myTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myTextView = findViewById(R.id.textView);
+        myImageView = findViewById(R.id.imageView);
+        findViewById(R.id.checkText).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.checkText:
+                if (myBitmap != null) {
+                    runTextRecog();
+                }
+                break;
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case WRITE_STORAGE:
+                    checkPermission(requestCode);
+                    break;
+                case SELECT_PHOTO:
+                    Uri dataUri = data.getData();
+                    String path = MyHelper.getPath(this, dataUri);
+                    if (path == null) {
+                        myBitmap = MyHelper.resizePhoto(photo, this, dataUri, myImageView);
+                    } else {
+                        myBitmap = MyHelper.resizePhoto(photo, path, myImageView);
+                    }
+                    if (myBitmap != null) {
+                        myTextView.setText(null);
+                        myImageView.setImageBitmap(myBitmap);
+                    }
+                    break;
+
+            }
+        }
+    }
+
+    private void runTextRecog() {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(myBitmap);
+        FirebaseVisionTextDetector detector = FirebaseVision.getInstance().getVisionTextDetector();
+        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText texts) {
+                processExtractedText(texts);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure
+                    (@NonNull Exception exception) {
+                Toast.makeText(MainActivity.this,
+                        "Exception", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void processExtractedText(FirebaseVisionText firebaseVisionText) {
+        myTextView.setText(null);
+        if (firebaseVisionText.getBlocks().size() == 0) {
+            myTextView.setText(R.string.no_text);
+            return;
+        }
+        for (FirebaseVisionText.Block block : firebaseVisionText.getBlocks()) {
+            myTextView.append(block.getText());
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/vogella/android/textrecognistion/MyHelper.java
@@ -0,0 +1,82 @@
+package com.vogella.android.textrecognistion;
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.graphics.BitmapFactory.decodeFile;
+import static android.graphics.BitmapFactory.decodeStream;
+
+public class MyHelper {
+    public static String getPath(Context context, Uri uri) {
+        String path = "";
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
+        int column_index;
+        if (cursor != null) {
+            column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            path = cursor.getString(column_index);
+            cursor.close();
+        }
+        return path;
+    }
+
+    public static File createTempFile(File file) {
+        File directory = new File(Environment.getExternalStorageDirectory().getPath() + "/com.jessicathornsby.myapplication");
+        if (!directory.exists() || !directory.isDirectory()) {
+
+            directory.mkdirs();
+        }
+        if (file == null) {
+            file = new File(directory, "orig.jpg");
+        }
+        return file;
+    }
+
+    public static Bitmap resizePhoto(File imageFile, Context context, Uri uri, ImageView view) {
+        BitmapFactory.Options newOptions = new BitmapFactory.Options();
+        try {
+            decodeStream(context.getContentResolver().openInputStream(uri), null, newOptions);
+            int photoHeight = newOptions.outHeight;
+            int photoWidth = newOptions.outWidth;
+
+            newOptions.inSampleSize = Math.min(photoWidth / view.getWidth(), photoHeight / view.getHeight());
+            return compressPhoto(imageFile, decodeStream(context.getContentResolver().openInputStream(uri), null, newOptions));
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Bitmap resizePhoto(File imageFile, String path, ImageView view) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        decodeFile(path, options);
+        int photoHeight = options.outHeight;
+        int photoWidth = options.outWidth;
+        options.inSampleSize = Math.min(photoWidth / view.getWidth(), photoHeight / view.getHeight());
+        return compressPhoto(imageFile, decodeFile(path, options));
+    }
+
+    private static Bitmap compressPhoto(File photoFile, Bitmap bitmap) {
+        try {
+            FileOutputStream fOutput = new FileOutputStream(photoFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 70, fOutput);
+            fOutput.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return bitmap;
+    }
+}
